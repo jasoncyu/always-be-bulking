a229fcbefc16b309ba1af6ad1c1593f17d57a951
jasoncyu <yujason2@gmail.com>
2016-06-17 16:15:10 -0700

    Remove TodoList stuff.

--- app/components/TodoListComponent/tests/index.test.js
+++
@@ -1,9 +0,0 @@
-import TodoListComponent from '../index';
-
-import expect from 'expect';
-import { shallow } from 'enzyme';
-import React from 'react';
-
-describe('<TodoListComponent />', () => {
-
-});
